6858aafb86ba4c328a830f53a3c1e4314bf00ba2
juanizq <juanizquierdoromero@gmail.com>
2023-09-03 12:57:22 +0200

    Ejercicio 3 (back)

--- packages/backend/src/utils/fetch-rates.js
+++ packages/backend/src/utils/fetch-rates.js
@@ -1,14 +1,16 @@
 import undici from "undici";
 import { config } from "../config.js";
 
-export const fetchRates = async () => {
+let fetchingRatesPromise = null;
+
+const fetchRatesFromApi = async () => {
   const endpoint = new URL(config.MONEYCONVERT_ENDPOINT);
-  // Prevent cached response.
   endpoint.searchParams.set("_", Date.now());
 
   const { statusCode, body } = await undici.request(endpoint, {
     method: "GET",
   });
+
   if (statusCode < 200 || statusCode > 299) {
     throw new Error(`Unexpected API response code: ${statusCode}`);
   }
@@ -20,3 +22,26 @@ export const fetchRates = async () => {
 
   return new Map([...Object.entries(data.rates)].sort());
 };
+
+export const fetchRates = async () => {
+
+  if (fetchingRatesPromise) {
+    return fetchingRatesPromise; 
+  }
+
+ 
+  fetchingRatesPromise = fetchRatesFromApi()
+    .then((rates) => {
+
+      fetchingRatesPromise = null;
+      return rates;
+    })
+    .catch((error) => {
+
+      fetchingRatesPromise = null;
+      throw error;
+    });
+
+  return fetchingRatesPromise;
+};
+
